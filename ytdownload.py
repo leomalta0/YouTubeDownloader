881ea2e655c160d0e9af0a5f1e1780e74602dc4a
Leonardo <leo.malta0@gmail.com>
2023-02-15 10:44:16 -0300

    Commit inicial

---
+++ ytdownload.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+
+def download(link):
+    video = YouTube(link)
+    video = video.streams.get_highest_resolution()
+    try:
+        video.download()
+    except:
+        print("Ocorreu um erro. Verifique o link utilizado.")
+    print("Download concluído. Cheque a pasta downloads para ver o seu vídeo.")
+
+link = input("Insira a URL do vídeo que você deseja fazer o download: ")
+download(link)
